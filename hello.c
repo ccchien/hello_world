a07b05590d774700653c2826a62366e99133df20
ycshu <ycshu@mail.ncku.edu.tw>
2015-03-11 09:53:22 +0800

    我會用數學函數了！

--- hello.c
+++ hello.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
 	printf("Hello World!\n");
 	printf("%d\n", 2+3);		// %d print an integer
 	printf("%f\n", 1.2+34.654); // %f print a float number
+	printf("%.16f\n", cos(2.0*M_PI/3.0)); 
 	return 123;
 }
