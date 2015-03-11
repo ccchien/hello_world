caa65ea1400b99212c5db458ad9f6a3c587ec1e9
ycshu <ycshu@mail.ncku.edu.tw>
2015-03-11 10:00:12 +0800

    我把自己的名字印進去了！

--- hello.c
+++ hello.c
@@ -2,8 +2,19 @@
 
 int main()
 {
+	char Name[50] = "Yu-Hsun Lee"; 
+	// Name is a fixed char vector 
+	// Name[0], Name[1], Name[2], ..., Name[49]
+	// Name[0] = Y
+	// Name[1] = u
+	// Name[2] = -
+	// Name[3] = H
+	// ...
+	// Name[11] = \0
 	printf("Hello World!\n");
 	printf("%d\n", 2+3);		// %d print an integer
 	printf("%f\n", 1.2+34.654); // %f print a float number
+	printf("%s\n", Name);
+	printf("%c\n", Name[5]);
 	return 123;
 }
